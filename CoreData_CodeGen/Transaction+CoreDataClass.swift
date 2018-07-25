88a5e112e9c6253d983a8b8ca0458506fc786c6f
adulphan youngmod <adulphan@hotmail.com>
2018-07-25 08:46:08 +0700

    ! and ?

---
+++ CoreData_CodeGen/Transaction+CoreDataClass.swift
@@ -0,0 +1,29 @@
+//
+//  Transaction+CoreDataClass.swift
+//  goldbac
+//
+//  Created by adulphan youngmod on 24/7/18.
+//  Copyright © 2018 goldbac Inc. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Transaction: NSManagedObject {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
