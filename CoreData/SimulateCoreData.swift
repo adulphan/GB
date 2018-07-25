4de39175f7e7bc5f47aa689d1f89fdc2f918e864
adulphan youngmod <adulphan@hotmail.com>
2018-07-25 08:47:19 +0700

    MMM

---
+++ CoreData/SimulateCoreData.swift
@@ -0,0 +1,41 @@
+//
+//  SimulateCoreData.swift
+//  goldbac
+//
+//  Created by adulphan youngmod on 25/7/18.
+//  Copyright © 2018 goldbac Inc. All rights reserved.
+//
+
+import Foundation
+
+extension CoreData {
+    
+    enum AccountType : Int {
+        
+        case cash = 0
+        case card = 1
+        case bank = 2
+        case expense = 3
+        case income = 4
+        case asset = 5
+        case debt = 6
+        case equity = 7
+    }
+    
+    func simulateAccounts() {
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
