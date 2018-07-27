4762d76af8a2de7e82219a8775cb0e9b6c71dfbd
adulphan youngmod <adulphan@hotmail.com>
2018-07-27 11:29:07 +0700

    test

--- goldbac/AppDelegate.swift
+++ goldbac/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+//testmerge
+    
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
