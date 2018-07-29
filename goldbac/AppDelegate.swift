905624541368268665db970a06f048c51acbf3ed
adulphan youngmod <adulphan@hotmail.com>
2018-07-29 11:30:54 +0700

    minor

--- goldbac/AppDelegate.swift
+++ goldbac/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        
+        // Master Branch
 
         let controller = UIViewController()
         controller.view.backgroundColor = UIColor.blue
